ad0da17d026d22ecb2127c20586fa997c2e3d9d5
ded1kowski <ameldedic69@gmail.com>
2017-10-06 20:04:39 +0200

    initial

--- app/src/main/java/com/example/ded1kowski/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ded1kowski/myapplication/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.ded1kowski.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.buttonSendMessage);
+        textView = (TextView) findViewById(R.id.textView);
+    }
+
+    public void changeText(View view) {
+        textView.setText("Amel Dedić");
     }
 }
